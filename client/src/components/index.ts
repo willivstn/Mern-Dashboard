9c8cbacaaefdeb6822dbac989ad071ca743c669d
Willie van Staden <60608218+willivstn@users.noreply.github.com>
2023-02-19 15:12:48 +0200

    first commit

---
+++ client/src/components/index.ts
@@ -0,0 +1,26 @@
+// common
+import Profile from './common/Profile';
+import PropertyCard from './common/PropertyCard';
+import CustomButton from './common/CustomButton';
+
+// charts
+import PieChart from './Charts/PieChart';
+import PropertyReferrals from './Charts/PropertyReferrals';
+import TotalRevenue from './Charts/TotalRevenue';
+
+// agent
+import AgentCard from './agent/AgentCard';
+
+// home
+import TopAgent from './home/TopAgent';
+
+export {
+  Profile,
+  PropertyCard,
+  CustomButton,
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  AgentCard,
+  TopAgent,
+};
---
+++ server/Routes/user.routes.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+import {createUser, getAllUsers, getUserInfoByID} from '../Controllers/user.controller.js';
+
+const router = express.Router();
+
+router.route('/').get(getAllUsers);
+router.route('/').post(createUser);
+router.route('/:id').get(getUserInfoByID);
+
+export default router;
